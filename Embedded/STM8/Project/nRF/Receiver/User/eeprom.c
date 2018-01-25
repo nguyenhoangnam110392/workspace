5e3705cb4b5911e15b728d1a16743e4eeadc55a5
nguyenhoangnam110392 <nguyenhoangnam110392@gmail.com>
2018-01-25 15:21:21 +0700

    update code eeprom

--- Embedded/STM8/Project/nRF/Receiver/User/eeprom.c
+++ Embedded/STM8/Project/nRF/Receiver/User/eeprom.c
@@ -1,21 +1,39 @@
-/*
- *******************************************************************************
- * File: eeprom.c                                                              *
- * Function: Read/write EEPROM. Use for STM8S103                               *
- * Parameter: None                                                             *
- * Returns: None                                                               *
- * Description: None                                                           *
- *******************************************************************************
- */
+/*============================================================================*/
+/* Library      : eeprom.c                                                    */
+/* Description  : Provide API to access EEPROM                                */
+/* Device       : STM8                                                        */
+/*============================================================================*/
+/*                                COPYRIGHT                                   */
+/*============================================================================*/
+/* Copyright(c) 2018 nguyenhoangnam110392@gmail.com                           */
+/*============================================================================*/
 
+/*******************************************************************************
+**                             Include Section                                **
+*******************************************************************************/
 #include "eeprom.h"
-
-/* Data to write into the EEPROM */
-static uint8_t data[9] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
-#define EEPROM_ADDRESS    0x4000
-#define EEPROM_FAILED     0x00
-#define EEPROM_OK         0x01
-
+/*******************************************************************************
+**                           Function Prototypes                              **
+*******************************************************************************/
+/*******************************************************************************
+**                           Variable Definition                              **
+*******************************************************************************/
+#define EEPROM_FLAG_DUL       (uint8_t)0x08   /*< Data EEPROM unlocked flag > */
+#define EEPROM_ADDRESS_START  0x4000          /*< EEPROM address start >      */
+#define EEPROM_ADDRESS_END    0x427F          /*< EEPROM address start >      */
+#define EEPROM_FAILED         0x01            /*< EEPROM return value >       */
+#define EEPROM_OK             0x00            /*< EEPROM return value >       */
+#define NUMBER_OF_DATA        0x07            /*< Number of array data >      */
+/*******************************************************************************
+**                           Variable Declaration                             **
+*******************************************************************************/
+  /*
+  * Description: Data will be write to EEPROM
+  * Bit 0: TX mode : 0 - RX mode : 1
+  * Bit 1 to 5: Address of node
+  * Bit 6: Checksum
+  */
+  uint8_t eeprom_data[NUMBER_OF_DATA] = {0};
 /*******************************************************************************
  * Name: EEPROM_Write_Value
  * Function: Write data to EEPROM
@@ -23,30 +41,29 @@ static uint8_t data[9] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09};
  * Returns: None
  * Description: None
  ******************************************************************************/
-void EEPROM_Write_Value(void)
+uint8_t EEPROM_Write_Value(uint8_t *ptr)
 {
-  uint8_t status;
-  status = FLASH->IAPSR & FLASH_FLAG_DUL;
+  /* Copy pointed data to local array */
+  memcpy(eeprom_data, ptr, NUMBER_OF_DATA);
   /* Check if the EEPROM is write-protected. If it is then unlock the EEPROM */
-  if (status == 0)
+  if (FLASH->IAPSR & EEPROM_FLAG_DUL == 0)
   {
-    FLASH->DUKR = 0xAE;     /* Warning: keys are reversed on data memory !!! */
-    FLASH->DUKR = 0x56;
+    FLASH->DUKR = 0xAE;     /* Write unlock key 1 */
+    FLASH->DUKR = 0x56;     /* Write unlock key 2 */
   }
 
-  /* Write the data to the EEPROM. */
-  uint8_t *address = (uint8_t*) EEPROM_ADDRESS + 7;  /*  EEPROM base address */
-  /*  Store number of data */
-  *address = 0x07;
-  //for (uint8_t index = 0; index < 7; index++)
-  //{
-      *address++ = data[0];
-      *address++ = data[1];
-      *address++ = data[2];
-      *address++ = data[3];
-  //}
+  /* Define pointer for EEPROM_ADDRESS */
+  uint8_t *address = (uint8_t*) EEPROM_ADDRESS;
+  /* Write data to EEPROM */
+  for (uint8_t index = 0; index < NUMBER_OF_DATA; index++)
+  {
+    *address++ = data[index];
+  }
   /* Now write protect the EEPROM */
-  FLASH->IAPSR = ~FLASH_IAPSR_DUL;
+  FLASH->IAPSR = (uint8_t)(~EEPROM_FLAG_DUL);
+
+  /* Verify written data in EEPROM */
+  return VerifyEEPROMData(ptr);
 }
 
 /*******************************************************************************
@@ -56,26 +73,24 @@ void EEPROM_Write_Value(void)
  * Returns: None
  * Description: None
  ******************************************************************************/
-uint8_t VerifyEEPROMData(void)
+uint8_t VerifyEEPROMData(uint8_t *ptr)
 {
-  uint8_t *address = (uint8_t*) EEPROM_ADDRESS;        //  EEPROM base address.
   uint8_t retVal = EEPROM_OK;
+  uint8_t original_data[NUMBER_OF_DATA] = {0};
+  
+  /* Copy pointed data to local array */
+  memcpy(original_data, ptr, NUMBER_OF_DATA);
 
-  if (*address++ != 7)
+  /* Define pointer for EEPROM_ADDRESS */
+  uint8_t *address = (uint8_t*) EEPROM_ADDRESS;
+  for (int index = 0; index < NUMBER_OF_DATA; index++)
   {
-    return EEPROM_FAILED;
-  }
-  else
-  {
-    for (int index = 0; index < 8; index++)
+    if (*address++ != original_data[index])
     {
-      uint8_t value = *address++;
-      if (value != data[index])
-      {
-        return EEPROM_FAILED;
-      }
+      return EEPROM_FAILED;
     }
   }
+  
   return retVal;
 }
-/***************************** END OF FILE ***********************************/
+/****************************** END OF FILE ***********************************/
